7c8a5da9e3d4c25db86bf243cb065b981b149458
AlexWang3 <43504208+AlexWang3@users.noreply.github.com>
2018-11-18 18:41:02 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+package TheMirror;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N=in.nextInt();
+		int []a=new int[N];
+		int []b=new int[N];
+		for (int i=0;i<N;i++) {
+			a[i]=in.nextInt();
+			b[i]=in.nextInt();
+		}
+		int count=0;
+		for(int i=0;i<N;i++) {
+			count=0;
+			for(int n=a[i];n<b[i];n++) {
+				if(IsPrime(n)) {
+					count++;
+				}
+			}
+			System.out.println(count);
+		}
+		
+	}
+	public static boolean IsPrime(int n) {
+		int i=2;
+		if(n==1)
+			return false;
+		while((n%i!=0)&&i*i<n) {
+			i++;
+		}
+		if(i*i>n)
+			return true;
+		return false;
+	}
+
+}
